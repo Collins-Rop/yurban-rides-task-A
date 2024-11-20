c5caa8a3bdaa801f47dca49966e944b816a0bf86
Collins-rop <colinrop68@gmail.com>
2024-11-20 09:34:15 +0300

    (feat)forgot password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -53,6 +53,9 @@
                             <a href="{{ route('register') }}" class="text-blue-600 hover:text-blue-700 font-medium text-sm">
                                 {{ __('Not registered? Sign up here') }}
                             </a>
+                            <a href="{{ route('password.request') }}" class="text-blue-600 hover:text-blue-700 font-medium text-sm">
+                                {{ __('Forgot your password?') }}
+                            </a>
                         </div>
                     @endif
                     </div>
